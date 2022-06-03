5ca0bca54ec2257fc6585ddb30685053c4037a49
Andrew Smith <andrewcs149@gmail.com>
2022-06-02 22:58:51 -0700

    Add display_menu() function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    display_menu();
+}
+
+fn display_menu() {
+    println!(
+        "Welcome!
+Please choose from the following options:
+
+1 - One Player
+2 - Two Player
+3 - Quit"
+    );
 }
