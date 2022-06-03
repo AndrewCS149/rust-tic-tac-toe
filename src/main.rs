e4b23dfa17bab93410bded5991b2b1cbe0b9b992
Andrew Smith <andrewcs149@gmail.com>
2022-06-02 23:20:33 -0700

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     let mut option;
 
     loop {
+        // get user input
         option = String::new();
-
         io::stdin()
             .read_line(&mut option)
             .expect("Failed to read line");
@@ -36,25 +36,26 @@ Please choose from the following options:
 /// Determines what functions to call based on the menu selection of the user
 fn menu_selection(option: u8) {
     match option {
-        1 => first_selection(),
-        2 => second_selection(),
+        1 => start_one_player_game(),
+        2 => start_two_player_game(),
         3 => exit_program(),
         _ => println!("Not a valid selection"),
     }
 }
 
 /// Initiates a one-player game
-fn first_selection() {
+fn start_one_player_game() {
     println!("one player!")
 }
 
 /// Initiates a two-player game
-fn second_selection() {
+fn start_two_player_game() {
     println!("two player!")
 }
 
 /// Exits the program
 fn exit_program() {
+    println!();
     println!("Thank you for playing!");
     println!("Shutting down...");
     exit(0)
